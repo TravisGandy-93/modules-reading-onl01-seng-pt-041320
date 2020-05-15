c40962a23fb403b4497dfafeaa6091c020265c69
Travis Gandy <travis_gandy@yahoo.com>
2020-05-15 18:36:59 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
   include Dance
- 
+  extend MetaDancing
   attr_accessor :name
  
   def initialize(name)
@@ -10,6 +10,3 @@ class Kid
   end
 end
 
-class Kid
-  extend MetaDancing
-end
